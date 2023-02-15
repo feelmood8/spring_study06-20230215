0fad56fa564d5b566ee331db1cdf3532b1f756a5
DESKTOP-LT5RG9T\ici <qwade1327@gmail.com>
2023-02-15 17:47:32 +0900

    에러

--- src/main/java/com/amu/home/dao/BDao.java
+++ src/main/java/com/amu/home/dao/BDao.java
@@ -1,7 +1,13 @@
 package com.amu.home.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import com.amu.home.dto.BDto;
@@ -16,6 +22,14 @@ public class BDao {
 		super();
 		// TODO Auto-generated constructor stub
 		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
 		
 	}
 
@@ -25,6 +39,57 @@ public class BDao {
 		
 		ArrayList<BDto> dtos = new ArrayList<BDto>();
 		
+		Connection conn= null; //DB 연결 생성
+		PreparedStatement pstmt =  null; //sql 실행 
+		ResultSet  rs = null; //select 일때 결과 받아주는 객체
+		
+		try {
+			
+			conn = dataSource.getConnection(); //dataSource에서 connection 생성
+			
+			String sql = "select * from mvc_board order by bid desc";
+			//게시글 번호의 내림차순 정렬로 모든 글 목록 가져오기(최근글이 가장 위에 오도록 함)
+			
+			pstmt = conn.prepareStatement(sql); // sql 실행
+			rs = pstmt.executeQuery(); // select 는 query 나머지는 update
+			System.out.println(rs.next());
+			while(rs.next()) { //rs에 들어있는 글들의 수만큼 반복
+				int bid = rs.getInt("bid");
+				String bname = rs.getString("bname");
+				String btitle = rs.getString("btitle");
+				String bcontent = rs.getString("bcontent");
+				Timestamp bdate = rs.getTimestamp("bdate");
+				int bhit = rs.getInt("bhit");
+				int bgroup = rs.getInt("bgroup");
+				int bstep = rs.getInt("bstep");
+				int bindent = rs.getInt("bindent");
+				
+			    System.out.println(btitle);
+	           BDto dto = new BDto(bid, bname,   bcontent, btitle, bdate, bhit, bgroup, bstep, bindent);
+	            dtos.add(dto);
+
+			}
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 		
 		return dtos;
--- src/main/webapp/WEB-INF/views/list.jsp
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -1,13 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>게시판 홈페이지</title>
+<title>list</title>
 </head>
 <body>
-	<h1><a href = "list">자유게시판 이동하기</a></h1>
-
+	<c:forEach items="${list }" var="bdto">
+	${bdto.btitle }<br>
+	
+	</c:forEach>
 </body>
 </html>
